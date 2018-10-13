bc10169a7b6df380082d8130d48ad57fcb4015b9
Marc Cataford <c.marcandre@gmail.com>
2018-10-12 20:44:33 -0400

    lint: minor

--- src/wrapper.js
+++ src/wrapper.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 import {
   MissingAPIKeyError,
   RateLimitExceededError,
-  UnsupportedOperatorError,
 } from './Exceptions'
 
 import QueryBuilder from './ODataQueryBuilder'
